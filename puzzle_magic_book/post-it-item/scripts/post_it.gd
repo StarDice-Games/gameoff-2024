b85c780b9d42170f20f96d7bdf99d521a6a9d40f
Guidi Simone <simone.guidi@tutanota.com>
2024-11-17 10:03:17 +0100

    + trigger for when a post-it is picked up

--- puzzle_magic_book/post-it-item/scripts/post_it.gd
+++ puzzle_magic_book/post-it-item/scripts/post_it.gd
@@ -1,15 +1,19 @@
 extends Node2D
 
 @export var item : ItemData
-
+@export var picked_up : String = "post-it_picked"
 @onready var label = $Label
 
 func _ready() -> void:
 	label.hide()
+	if TriggersSystem.check_trigger(picked_up, true):
+		hide()
+		process_mode = ProcessMode.PROCESS_MODE_DISABLED
 
 func _on_interactable_interacted() -> void:
 	print("player interaction with", name)
 	InventorySystem.pick_up(item)
+	TriggersSystem.update_trigger(picked_up, true)
 	hide()
 	process_mode = ProcessMode.PROCESS_MODE_DISABLED
 
