96d5606b98ae9e50728d7c840e8ff5c54a1db3d9
KevinToninelli <kevintoninelli5@gmail.com>
2024-11-17 14:59:13 +0100

    + first cut scene

--- levels/first_day_cutscene/scripts/first_day_cutscene.gd
+++ levels/first_day_cutscene/scripts/first_day_cutscene.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@export var dialog_1 : Array[DialogText]
+@export var dialog_2 : Array[DialogText]
+@export var dialog_3 : Array[DialogText]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,6 +11,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("interact"):
-		LevelSystem.load_level("guardiola", true)
-	pass
+	if DialogueSystem.started == false and $Timer.is_stopped():
+		$AnimationPlayer.play("walk_inside")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	LevelSystem.load_level("guardiola", true)
+
+func _on_timer_timeout() -> void:
+	DialogueSystem.start_dialog(dialog_1)
