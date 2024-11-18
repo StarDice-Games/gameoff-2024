85fede5dce02dc18ee6ad358d851e08b8d28b1d5
KevinToninelli <kevintoninelli5@gmail.com>
2024-11-18 21:45:04 +0100

    + act 2 flow

--- levels/guardiola/guardiola.gd
+++ levels/guardiola/guardiola.gd
@@ -2,16 +2,25 @@ extends Node2D
 
 @export var first_tasks : Array[Task]
 
+var close_museum = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if TriggersSystem.check_trigger("first_boss_call", false):
 		TriggersSystem.update_trigger("ring", true)
 	
 	if first_tasks[0].current_counter == first_tasks[0].counter_max:
+		TriggersSystem.update_trigger("act_2", true)
 		TriggersSystem.update_trigger("ring", true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if TriggersSystem.check_trigger("first_boss_call", true):
 		TaskSystem.load_tasks(first_tasks)
+	
+	if TriggersSystem.check_trigger("close_museum", true):
+		LevelSystem.load_level("close_museum_cutscene", true)
+		
+	if Input.is_action_just_pressed("debug_kev"):
+		first_tasks[0].current_counter = 4
 	
--- levels/splash_screen/splash_screen.gd
+++ levels/splash_screen/splash_screen.gd
@@ -1,18 +1,14 @@
 extends Control
 
-signal end_splash
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	EventSystem.hide_hud.emit()
-	$AnimationPlayer.play("Splash")
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func endSpashScreen():
-	#end_splash.emit()
-	LevelSystem.load_level("main menu")
+
+func _on_timer_timeout() -> void:
+	TriggersSystem.update_trigger("close_museum", false)
+	LevelSystem.load_level("guardiola", true)
