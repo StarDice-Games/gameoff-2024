6ecdfbc28062d500423d23ca734b04213d5ad892
Guidi Simone <simone.guidi@tutanota.com>
2024-11-19 22:30:42 +0100

    + fixed dialogs bug

--- levels/guardiola/guardiola.gd
+++ levels/guardiola/guardiola.gd
@@ -8,17 +8,17 @@ extends Node2D
 var second_task_loaded = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	EventSystem.trigger_changed.connect(trigger_update)
+	
 	TriggersSystem.update_trigger("doors_locked", true)
 		
 	if TriggersSystem.check_trigger("act_1", false):
 		TriggersSystem.update_trigger("ring", true)
 	
 	# Prima chiamata con il boss e task NPC
-	if first_tasks.size() != 0:
-		if first_tasks[0].current_counter == first_tasks[0].counter_max:
-			first_tasks.clear()
-			TriggersSystem.update_trigger("act_2", true)
-			TriggersSystem.update_trigger("ring", true)
+	if TaskSystem.check_all_task_completed() and TriggersSystem.check_trigger("act_1", true):
+		TriggersSystem.update_trigger("act_2", true)
+		TriggersSystem.update_trigger("ring", true)
 	
 	if TriggersSystem.check_trigger("act_3", true):
 		TriggersSystem.update_trigger("ring", true)
@@ -27,6 +27,10 @@ func _ready() -> void:
 		TriggersSystem.update_trigger("act_4", true)
 		TriggersSystem.update_trigger("ring", true)
 
+func trigger_update(key, value):
+	if key == "close_museum" and value == true:
+		LevelSystem.load_level("close_museum_cutscene", true)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if TriggersSystem.check_trigger("first_boss_call", true):
@@ -34,8 +38,8 @@ func _process(delta: float) -> void:
 		TriggersSystem.update_trigger("place_npc", true)
 		TriggersSystem.update_trigger("first_boss_call", false)
 	
-	if TriggersSystem.check_trigger("close_museum", true):
-		LevelSystem.load_level("close_museum_cutscene", true)
+	#if TriggersSystem.check_trigger("close_museum", true):
+		#LevelSystem.load_level("close_museum_cutscene", true)
 		
 	if TriggersSystem.check_trigger("dialog_player", true):
 		DialogueSystem.start_dialog(dialog_player)
@@ -47,7 +51,11 @@ func _process(delta: float) -> void:
 		TriggersSystem.update_trigger("act_3", false)
 		
 	if Input.is_action_just_pressed("debug_kev1"):
-		first_tasks[0].current_counter = 4
+		TaskSystem.update_task_counter(first_tasks[0].id)
+		TaskSystem.update_task_counter(first_tasks[0].id)
+		TaskSystem.update_task_counter(first_tasks[0].id)
+		TaskSystem.update_task_counter(first_tasks[0].id)
+		#first_tasks[0].current_counter = 4
 	
 	if Input.is_action_just_pressed("debug_kev2"):
 		second_tasks[0].complete = true
