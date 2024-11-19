b37d32be8e5baa8d0316b45fc138420d01307b99
Guidi Simone <simone.guidi@tutanota.com>
2024-11-19 22:02:13 +0100

    + resotred splash screen

--- levels/splash_screen/splash_screen.gd
+++ levels/splash_screen/splash_screen.gd
@@ -1,15 +1,18 @@
 extends Control
 
+signal end_splash
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	EventSystem.hide_hud.emit()
+	$AnimationPlayer.play("Splash")
 	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
-func _on_timer_timeout() -> void:
-	TriggersSystem.update_trigger("close_museum", false)
-	TriggersSystem.update_trigger("act_3", true)
-	LevelSystem.load_level("guardiola", true)
+func endSpashScreen():
+	#end_splash.emit()
+	LevelSystem.load_level("main menu")
