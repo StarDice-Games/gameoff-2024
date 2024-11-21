8659a5a5b56806c843433471ea9d164374e1ab43
KevinToninelli <kevintoninelli5@gmail.com>
2024-11-21 15:15:07 +0100

    fix t rex puzzle

--- act2-t-rex-puzzle/t-rex-trigger/script/t_rex_trigger.gd
+++ act2-t-rex-puzzle/t-rex-trigger/script/t_rex_trigger.gd
@@ -1,7 +1,8 @@
 extends Node2D
 @onready var label_text = $StaticBody2D/Label
-@onready var t_rex_tooth = $StaticBody2D/TrexTooth
+@onready var t_rex_tooth = $StaticBody2D/TrexToothSprite
 @onready var t_rex_tooth_rope = $StaticBody2D/TrexToothRope
+
 @export var dialog : Array[DialogText]
 @export var item : ItemData
 @export var audio_sfx = AudioStream
@@ -14,6 +15,16 @@ func _ready() -> void:
 	EventSystem.picked_up_item.connect(picked_rope)
 	EventSystem.dropped_item.connect(dropped_rope)
 	
+	if TriggersSystem.check_trigger("can_pool", true):
+		t_rex_tooth.hide()
+		t_rex_tooth_rope.show()
+		label_text.text = "Pull"
+		
+	if TriggersSystem.check_trigger("pulled_out", true):
+		$Interactable.queue_free()
+		t_rex_tooth_rope.hide()
+		if TriggersSystem.check_trigger("tooth_picked", false):
+			$StaticBody2D/TRexTooth.show()
 
 func picked_rope(item_id : String):
 	if item_id == item.id:
@@ -37,15 +48,16 @@ func _on_interactable_interacted() -> void:
 			t_rex_tooth_rope.show()
 			AudioSystem.play(audio_sfx)
 			label_text.text = "Pull"
+			TriggersSystem.toggle_trigger("can_pool")
 			can_pull = true
 			InventorySystem.drop_item(item)
 		else:
 			can_pull = false
-			t_rex_tooth.show()
 			t_rex_tooth_rope.hide()
 			TriggersSystem.toggle_trigger("pulled_out")
 			$Interactable.queue_free()
 			label_text.hide()
+			$StaticBody2D/TRexTooth.show()
 	else:
 		DialogueSystem.start_dialog(dialog)
 		
