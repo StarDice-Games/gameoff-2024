62487afca901f40f13c71462cdda31666d8fa287
Guidi Simone <simone.guidi@tutanota.com>
2024-11-21 13:49:24 +0100

    + fixed highlight component

--- highlight_component/ItemHighlight.gd
+++ highlight_component/ItemHighlight.gd
@@ -5,38 +5,43 @@ extends Node2D
 @export var collider_size: Vector2 = Vector2(100, 100)           # Default collider size
 @export var sprite : Sprite2D
 @export var interactable : Interactable
- 
-@onready var shader_material = sprite.material
-@onready var collision_shape = interactable.get_node("CollisionShape2D")
+@export var shader : ShaderMaterial
+
+#@onready var shader_material = sprite.material
+#@onready var collision_shape = interactable.get_node("CollisionShape2D")
 
 
 func _ready():
 	# Debugging outputs
 	print("[Debug] Sprite2D exists: ", sprite)
-	print("[Debug] Shader material: ", shader_material)
-	print("[Debug] CollisionShape2D exists: ", collision_shape)
+	print("[Debug] Shader material: ", shader)
+	#print("[Debug] CollisionShape2D exists: ", collision_shape)
 	
-	# Ensure the Sprite2D has a ShaderMaterial
-	if shader_material is ShaderMaterial:
-		shader_material.set_shader_parameter("line_color", highlight_color)
-		shader_material.set_shader_parameter("line_thickness", 0.0)  
+	if shader is ShaderMaterial:
+		shader.set_shader_parameter("line_color", highlight_color)
+		shader.set_shader_parameter("line_thickness", 0.0)  
 	else:
 		print("Error: Material is not a ShaderMaterial.")
-
+	
+	# Ensure the Sprite2D has a ShaderMaterial
+	if sprite != null:
+		sprite.material = shader
+		
+	interactable.player_enter.connect(interactable_player_enter)
+	interactable.player_exit.connect(interactable_player_exit)
+	
 	# Configure the CollisionShape2D
-	if collision_shape.shape is RectangleShape2D:
-		collision_shape.shape.extents = collider_size / 2  # Set size dynamically
-		print("[Debug] Collider size set to: ", collider_size)
-	else:
-		print("Error: CollisionShape2D does not use RectangleShape2D!")
+	#if collision_shape.shape is RectangleShape2D:
+		#collision_shape.shape.extents = collider_size / 2  # Set size dynamically
+		#print("[Debug] Collider size set to: ", collider_size)
+	#else:
+		#print("Error: CollisionShape2D does not use RectangleShape2D!")
 
-func _on_interactable_player_enter() -> void:
+func interactable_player_enter() -> void:
 	print("[Debug] Mouse entered the area")  
-	if shader_material is ShaderMaterial:
-		shader_material.set_shader_parameter("line_thickness", highlight_thickness)
-		shader_material.set_shader_parameter("line_color", highlight_color)
+	shader.set_shader_parameter("line_thickness", highlight_thickness)
+	shader.set_shader_parameter("line_color", highlight_color)
 
-func _on_interactable_player_exit() -> void:
-	print("[Debug] Mouse exited the area") 
-	if shader_material is ShaderMaterial:
-		shader_material.set_shader_parameter("line_thickness", 0.0)
+func interactable_player_exit() -> void:
+	print("[Debug] Mouse exited the area")
+	shader.set_shader_parameter("line_thickness", 0.0)
