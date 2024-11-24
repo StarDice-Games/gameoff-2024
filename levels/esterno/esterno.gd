da88622a8eb85277c1dc759b541683659ee22cfb
KevinToninelli <kevintoninelli5@gmail.com>
2024-11-24 01:27:21 +0100

    bug fix

--- levels/esterno/esterno.gd
+++ levels/esterno/esterno.gd
@@ -5,7 +5,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if TriggersSystem.check_trigger("close_museum", true):
-		$AnimationPlayer.play("fade_out")
+		$Node2D/AnimationPlayer.play("fade_out")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
