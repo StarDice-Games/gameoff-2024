7ab93e48befaabd6a244d97c284794c91c855201
Guidi Simone <simone.guidi@tutanota.com>
2024-11-24 09:18:24 +0100

    + highlight sprites when the player enters it's area

--- highlight_component/ItemHighlight.gd
+++ highlight_component/ItemHighlight.gd
@@ -1,10 +1,10 @@
-extends Node2D 
+extends Area2D 
 
 @export var highlight_color: Color = Color(1.0, 1.0, 1.0, 0.25)  # Highlight color
 @export var highlight_thickness: float = 3.0                     # Outline thickness
 @export var collider_size: Vector2 = Vector2(100, 100)           # Default collider size
 @export var sprite : Sprite2D
-@export var interactable : Interactable
+#@export var interactable : Interactable
 @export var shader : ShaderMaterial
 
 #@onready var shader_material = sprite.material
@@ -26,16 +26,6 @@ func _ready():
 	# Ensure the Sprite2D has a ShaderMaterial
 	if sprite != null:
 		sprite.material = shader
-		
-	interactable.player_enter.connect(interactable_player_enter)
-	interactable.player_exit.connect(interactable_player_exit)
-	
-	# Configure the CollisionShape2D
-	#if collision_shape.shape is RectangleShape2D:
-		#collision_shape.shape.extents = collider_size / 2  # Set size dynamically
-		#print("[Debug] Collider size set to: ", collider_size)
-	#else:
-		#print("Error: CollisionShape2D does not use RectangleShape2D!")
 
 func interactable_player_enter() -> void:
 	print("[Debug] Mouse entered the area")  
@@ -45,3 +35,21 @@ func interactable_player_enter() -> void:
 func interactable_player_exit() -> void:
 	print("[Debug] Mouse exited the area")
 	shader.set_shader_parameter("line_thickness", 0.0)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	var father = area.get_parent()
+	if father == null:
+		return
+	if father is Player :
+		shader.set_shader_parameter("line_thickness", highlight_thickness)
+		shader.set_shader_parameter("line_color", highlight_color)
+
+
+func _on_area_exited(area: Area2D) -> void:
+	var father = area.get_parent()
+	if father == null:
+		return
+	if father is Player :
+		shader.set_shader_parameter("line_thickness", 0.0)
+	pass # Replace with function body.
