21af6392a533ee277ab68e459e2b6231348c7d03
KevinToninelli <kevintoninelli5@gmail.com>
2024-12-06 08:54:26 +0100

    fix door lock

--- door_lock/door_lock.gd
+++ door_lock/door_lock.gd
@@ -2,12 +2,25 @@ extends Area2D
 
 @export var dialog : Array[DialogText]
 
+@onready var trigger: CollisionShape2D = $Trigger
+@onready var collision_shape_2d_2: CollisionShape2D = $StaticBody2D/CollisionShape2D2
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	EventSystem.trigger_changed.connect(update_trigger)
+	if TriggersSystem.check_trigger("doors_locked", false):
+		trigger.disabled = true
+		collision_shape_2d_2.disabled = true
 
+func update_trigger(key: String, value : bool) -> void:
+	if key == "doors_locked" and value == true:
+		trigger.disabled = false
+		collision_shape_2d_2.disabled = false
+	
+	if key == "doors_locked" and value == false:
+		trigger.disabled = true
+		collision_shape_2d_2.disabled = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
